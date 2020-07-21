4460222e1a917e3ddbafba0116535513b3285fd9
MskAdr <37520690+MskAdr@users.noreply.github.com>
2020-07-21 11:30:07 +0800

    Update 169.cpp

--- Week31/169.cpp
+++ Week31/169.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int count = 0;
-		int result;
-		for (int num:nums) {
+	int result;
+	for (int num:nums) {
             result = count == 0 ? num : result;
             count += result == num ? 1 : -1;
-		}
-		return result;
+	}
+	return result;
     }
 };
